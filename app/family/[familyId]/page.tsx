1ac2cdd4ba43dfb9786192d3f36f1a4463b2f79b
ymin431 <ymin431@naver.com>
2025-12-04 23:12:38 +0900

    feat: 가족 메뉴판에서 초대코드 API 구현 및 연결

--- app/family/[familyId]/page.tsx
+++ app/family/[familyId]/page.tsx
@@ -183,7 +183,7 @@ export default function FamilyDetailPage() {
         isOpen={isInviteModalOpen}
         onClose={() => setIsInviteModalOpen(false)}
         familyName={familyName}
-        inviteCode="FAM2024XYZ"
+        familyId={Number(familyIdParam)}
       />
 
       {/* 가족 구성원 관리 모달 */}
